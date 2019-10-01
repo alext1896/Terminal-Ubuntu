889bf4a48e167857e16192e96860cb17bd92f8d8
alejandro <alejandro@alejandro-VirtualBox>
2019-10-01 15:04:06 +0200

    Comandos top, cat, info, mkdir terminados.
    Falta que el método write no borre el texto que ya estaba escrito en el
    fichero

--- Terminal-master/src/javanio/Commands.java
+++ Terminal-master/src/javanio/Commands.java
@@ -1,23 +1,21 @@
 package javanio;
 
 
-import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.*;
+import java.nio.channels.FileChannel;
 import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
+import java.nio.file.*;
 
 public class Commands {
 	
-	Path rutaInicial = null;
+	private Path rutaInicial = null;
+	
 	public Commands () {
-		rutaInicial = Paths.get("/home/alejandro");
+		rutaInicial = Paths.get("/home");
 		
 	}
 	
-	public String rutaHome () {
+	public String rutaInterfaz () {
 		String rutaHome = "jose.guapache@Linux:" + rutaInicial.getName(rutaInicial.getNameCount()-1) + "~$ ";
 		return rutaHome;
 		
@@ -34,66 +32,6 @@ public class Commands {
 			this.rutaInicial = nuevaPath; 
 		}
 
-	}
-	
-	/*
-	 * @param nombreCarpeta
-	 * 
-	 * Este método recibe un String con el nombre de la carpeta que se quiere acceder.
-	 * A continuación crea una ruta a partir de la ruta home más el nombre de la carpeta, creando la Path de la
-	 * carpeta a la que se quiere acceder.
-	 * 
-	 * @return Devuelve la ruta de tipo Path de la carpeta que se quiere acceder. 
-	 */
-	public Path cd (String nombreCarpeta) {
-		Commands ruta = new Commands ();
-		String [] separacion = ruta.rutaHome().split(":");
-		String newPath = separacion [0] + "~/" + nombreCarpeta + "$";
-		Path nuevaRuta = Paths.get(newPath);
-		
-		return nuevaRuta;
-	}
-	
-	public Path cdAbsouluta (String rutaAbsoluta) {
-		Commands ruta = new Commands ();
-		String [] separacion = ruta.rutaHome().split(":");
-		String [] separacionRuta = rutaAbsoluta.split("alejandro");
-		
-		String newPath = separacion [0] + ":alejandro" + separacionRuta [1]+ "~$";
-		Path nuevaRuta = Paths.get(newPath);
-		return nuevaRuta;
-	}
-	
-	
-	/**
-	 * 
-	 * @return
-	 */
-	public Path cdPadre () {
-		Commands ruta = new Commands ();
-		
-		String [] pantalla = ruta.rutaHome().split(":");
-		String newPath = pantalla [0] + "~/"  + "$";
-		Path nuevaRuta = Paths.get(newPath);
-		return nuevaRuta;
-		
-		
-	}
-	
-	/**
-	 * @param ruta
-	 * Recibe un parámetro de tipo Path el cual lista los directorios y ficheros de la ruta en un array 
-	 * con el metodo .list().
-	 * 
-	 * por último, se imprime en pantalla el array.
-	 */
-	public void dir () {
-		String [] listar = rutaInicial.toFile().list();
-		
-		for (int i = 0; i < listar.length; i++) {
-			System.out.println(listar[i]);
-		}
-		
 	}
 	
 	public void help () throws IOException {
@@ -114,5 +52,195 @@ public class Commands {
 		}
 	}
 	
+	/*
+	 * @param nombreCarpeta
+	 * 
+	 * Este método recibe un String con el nombre de la carpeta que se quiere acceder.
+	 * A continuación crea una ruta a partir de la ruta home más el nombre de la carpeta, creando la Path de la
+	 * carpeta a la que se quiere acceder.
+	 * 
+	 * @return Devuelve la ruta de tipo Path de la carpeta que se quiere acceder. 
+	 */
+	public Path cd (String nombreCarpeta) {
+		Commands ruta = new Commands ();
+		String [] separacion = ruta.rutaInterfaz().split(":");
+		String newPath = separacion [0] + "~/" + nombreCarpeta + "$";
+		Path nuevaRuta = Paths.get(newPath);
+		
+		return nuevaRuta;
+	}
+	
+	public Path cdAbsouluta (String rutaAbsoluta) {
+		Commands ruta = new Commands ();
+		String [] separacion = ruta.rutaInterfaz().split(":");
+		String [] separacionRuta = rutaAbsoluta.split("alejandro");
+		
+		String newPath = separacion [0] + ":alejandro" + separacionRuta [1]+ "~$";
+		Path nuevaRuta = Paths.get(newPath);
+		return nuevaRuta;
+	}
+	
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public Path cdPadre () {
+		Commands ruta = new Commands ();
+		
+		String [] pantalla = ruta.rutaInterfaz().split(":");
+		String newPath = pantalla [0] + "~/"  + "$";
+		Path nuevaRuta = Paths.get(newPath);
+		return nuevaRuta;
+	}
+	
+	/**
+	 * @param ruta
+	 * Recibe un parámetro de tipo Path el cual lista los directorios y ficheros de la ruta en un array 
+	 * con el metodo .list().
+	 * 
+	 * por último, se imprime en pantalla el array.
+	 */
+	public void dir () {
+		String [] listar = rutaInicial.toFile().list();
+		
+		for (int i = 0; i < listar.length; i++) {
+			System.out.println(listar[i]);
+		}
+	}
+	
+	public void mkdir (String nombreArchivo) {
+		Path rutaArchivo = Paths.get(rutaInicial.toAbsolutePath() + "/"+nombreArchivo);
 
+		if (rutaArchivo.toFile().isDirectory() == true) {
+			System.out.println("mkdir: no se puede crear el directorio " + nombreArchivo +" El archivo ya existe");
+			
+		}else {
+			rutaArchivo.toFile().mkdir();
+			
+		}
+	}
+	
+	public void info (String nombreArchivo) throws IOException {
+		Path rutaArchivo = Paths.get(rutaInicial.toAbsolutePath() + "/" + nombreArchivo);
+		FileChannel canal = FileChannel.open(rutaArchivo);
+		long tamañoArchivo = canal.size();
+		
+		if (rutaArchivo.toFile().isDirectory() || rutaArchivo.toFile().isFile()) {
+			System.out.println("Nombre del archivo: " + nombreArchivo);
+			System.out.println("Nombre del archivo padre: " + rutaInicial.toAbsolutePath().getName(rutaInicial.getNameCount()-1));
+			System.out.println("Tamaño del archivo: " + tamañoArchivo + "Bytes");
+		}else {
+			System.out.println("El archivo no existe");
+		}
+	}
+	
+	public void cat (String nombreFichero) {
+		Path rutaFichero = Paths.get(rutaInicial.toAbsolutePath()+ "/" + nombreFichero);
+		Charset charset = Charset.forName("ISO-8859-1");
+		BufferedReader br = null;
+		
+		if (rutaFichero.toFile().isFile()) {
+			try {
+				br = Files.newBufferedReader(rutaFichero, charset);
+				String linea = null;
+				
+				while ((linea = br.readLine()) != null) {
+					System.out.println(linea);
+					}
+				
+				br.close();
+				} catch (IOException x) {
+					System.err.format("IOException: %s%n", x);
+			}
+		}else {
+			System.out.println(nombreFichero + ": Es un directorio");
+		}
+	}
+
+	/**
+	 * Muestras las primeras <numlineas> de un fichero.	De manera que top 20 hola.txt muestra las primeras 20 líneas del fichero hola.txt
+	 * @param numLineas
+	 * @param nombreFichero
+	 */
+	public void top (String numLineas, String nombreFichero) {
+		try {
+			Integer numeroLineas = Integer.parseInt(numLineas);
+			int contador = 0;
+			
+			Path rutaFichero = Paths.get(rutaInicial.toAbsolutePath()+ "/" + nombreFichero);
+			Charset charset = Charset.forName("ISO-8859-1");
+			BufferedReader br = null;
+			
+			 if (rutaFichero.toFile().exists()) {
+				 if (rutaFichero.toFile().isFile()) {
+						try {
+							br = Files.newBufferedReader(rutaFichero, charset);
+							String linea = null;
+							
+							while ((linea = br.readLine()) != null && contador < numeroLineas) {
+								System.out.println(linea);
+								contador++;
+								}
+							
+							br.close();
+							} catch (IOException x) {
+								System.err.format("IOException: %s%n", x);
+						}
+					}else {
+						System.out.println(nombreFichero + ": Es un directorio");
+					}
+				}else {
+					System.out.println("El archivo al que quieres acceder no existe.");
+			}
+		}catch (NumberFormatException e) {
+			System.out.println("El segundo valor debe ser un número");
+			System.out.println("Por ejemplo: top 5 hola. Siendo hola el nombre del archivo .txt");
+		}
+	}
+	
+	public void write (String nombreFichero, String [] texto) {
+		Path rutaFichero = Paths.get(rutaInicial.normalize() + "/" + nombreFichero);
+		Charset charset = Charset.forName("ISO-8859-1");
+		try (BufferedWriter wr = Files.newBufferedWriter(rutaFichero, charset)) {
+			
+			for (int i = 2; i < texto.length; i++) {
+				wr.write(texto[i] + " ");
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			System.out.println("El archivo no exite");
+		}
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Terminal-master/src/javanio/mainTerminalNio.java
+++ Terminal-master/src/javanio/mainTerminalNio.java
@@ -4,18 +4,19 @@ import java.io.*;
 import java.util.Scanner;
 
 public class mainTerminalNio {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		Scanner sc = new Scanner(System.in);
 		String operacion = "";
 		Commands comandos = new Commands ();
 				
-		System.out.println(comandos.rutaHome());
+		System.out.println(comandos.rutaInterfaz());
 		operacion = sc.nextLine();
 
 		do {
 			
 			String [] separacion = operacion.split(" ");
 			
+			//Comando help
 			if (separacion[0].equals("help")) {
 				try {
 					comandos.help();
@@ -24,30 +25,70 @@ public class mainTerminalNio {
 				}
 			}
 			
-			if (separacion[0].equals("cd")) {
-				if (separacion [1].startsWith("/")){
-					System.out.println(comandos.cdAbsouluta(separacion [1].toString()));
-					comandos.setRutaInicial(separacion[1]);;
-					
-				}else if (separacion [1].startsWith("..")) {
-					System.out.println(comandos.cdPadre().toString());
-					comandos.setRutaInicial(separacion[1]);
-				}
-				
-				
-				else {
-					System.out.println(comandos.cd(separacion[1].toString()));
-					comandos.setRutaInicial(separacion[1]);
-				}
+			//Comando cd
+			try {
+					if (separacion[0].equals("cd")) {
+						
+	
+					if (separacion [1].startsWith("/")){
+						System.out.println(comandos.cdAbsouluta(separacion [1].toString()));
+						comandos.setRutaInicial(separacion[1]);;
+						
+					}else if (separacion [1].startsWith("..")) {
+						System.out.println(comandos.cdPadre().toString());
+						comandos.setRutaInicial(separacion[1]);
+						
+					}else {
+						System.out.println(comandos.cd(separacion[1].toString()));
+						comandos.setRutaInicial(separacion[1]);
+					}
+				}	
+			}catch (ArrayIndexOutOfBoundsException e) {
+				comandos.setRutaInicial(separacion[1]);
+				System.out.println(comandos.cdPadre().toString());
 				
 			}
-		
+			
+			
+			//Comando dir
 			if (separacion[0].equals("dir")) {
 				comandos.dir();
-				comandos.rutaHome();
+				System.out.println(comandos.rutaInterfaz());
 			}
-			operacion = sc.nextLine();
 			
+			//Comando mkdir
+			if (separacion [0].equals("mkdir")) {
+				comandos.mkdir(separacion[1].toString());
+				System.out.println(comandos.rutaInterfaz());
+			}
+			
+			//Comando info
+			if (separacion [0].equals("info")) {
+				comandos.info(separacion[1]);
+				System.out.println(comandos.rutaInterfaz());
+			}
+			
+			//Comando cat
+			if (separacion [0].equals("cat")) {
+				comandos.cat(separacion [1]);
+				System.out.println(comandos.rutaInterfaz());
+			}
+			
+			//Comando top
+			if (separacion [0].equals("top")) {
+				comandos.top(separacion [1], separacion [2]);
+				System.out.println(comandos.rutaInterfaz());
+			}
+			
+			//Comando write
+			if (separacion [0].equals("write")){
+				comandos.write(separacion [1], separacion);
+				System.out.println(comandos.rutaInterfaz());
+				
+			}
+			
+			
+			operacion = sc.nextLine();
 		}while (operacion.equalsIgnoreCase("exit") == false);
 		
 		sc.close();
