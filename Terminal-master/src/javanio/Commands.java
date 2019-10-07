225686a77bd5189c0dd5872470a5d089b2ea1911
alejandro <alejandro@alejandro-VirtualBox>
2019-10-07 22:32:11 +0200

    Terminado

--- Terminal-master/src/javanio/Commands.java
+++ Terminal-master/src/javanio/Commands.java
@@ -1,25 +1,36 @@
 package javanio;
 
-
 import java.io.*;
 import java.nio.channels.FileChannel;
 import java.nio.charset.Charset;
 import java.nio.file.*;
-
+/**
+ * @author jose.guapache
+ *Esta clase tiene algunos de las funciones que tiene la terminal. Funciones como help, dir, info, cat, etc...
+ */
 public class Commands {
-	
-	private static final CopyOption REPLACE_EXISTING = null;
 	private Path rutaInicial = null;
 	
+	/**
+	 * El único atributo que tiene el constructor es la ruta inicial, la cual se inicializa con la ruta Home 
+	 */
 	public Commands () {
 		rutaInicial = Paths.get("/home");
-		
 	}
 	
+	/**
+	 * @return Devuelve una cadena de texto con la ruta en la que se encuentra el programa
+	 * Este método es simplemente para que se vea como la terminal de Linux
+	 * 
+	 */
 	public String rutaInterfaz () {
-		String rutaHome = "jose.guapache@Linux:" + rutaInicial.getName(rutaInicial.getNameCount()-1) + "~$ ";
-		return rutaHome;
-		
+		if (rutaInicial.toString().equals("/")) {
+			String rutaHome = "jose.guapache@Linux:" + "/" + "~$ ";
+			return rutaHome;
+		}else {
+			String rutaHome =  "jose.guapache@Linux:" + rutaInicial.getName(rutaInicial.getNameCount()-1) + "~$ ";
+			return rutaHome;
+		}
 	}
 	
 	/*
@@ -29,61 +40,90 @@ public class Commands {
 	 * A continuación crea una ruta a partir de la ruta home más el nombre de la carpeta, creando la Path de la
 	 * carpeta a la que se quiere acceder y cambiando la variable rutaInicial.
 	 * 
+	 * Este método funciona tanto para rutas absolutas como para volver una carpeta atras pasando como argumento ..
 	 */
-	public void setRutaInicial (String ruta) {
-		if (ruta.startsWith("/")) {
-		Path nuevaPath = Paths.get(ruta);
+	public void setRutaInicial (String nombreCarpeta) {
+		if (nombreCarpeta.startsWith("/")) {
+		Path nuevaPath = Paths.get(nombreCarpeta);
 		
 			if (nuevaPath.toFile().exists()) {
 				this.rutaInicial = nuevaPath;
 			}else {
-				System.out.println("cd: " + ruta + " No existe el archivo o el directorio");
+				System.out.println("cd: " + nombreCarpeta + " No existe el archivo o el directorio");
 			}
 			
-		}else if (ruta.startsWith("..")) {
+		}else if (nombreCarpeta.equals("..")) {
 			Path ultimaCarpeta = rutaInicial.normalize().getName(rutaInicial.getNameCount()-1);
-			String [] newPath = rutaInicial.toString().split("/" + ultimaCarpeta.toString());
-			Path nuevaPath = Paths.get(newPath[0]);
 			
-			if (nuevaPath.toFile().exists()) {
+			if (ultimaCarpeta.toString().equals("home")) {
+				Path nuevaPath = Paths.get("/");
 				this.rutaInicial = nuevaPath;
 			}else {
-				System.out.println("cd: " + ruta + " No existe el archivo o el directorio");
+				String [] newPath = rutaInicial.toString().split("/" + ultimaCarpeta.toString());
+				Path nuevaPath = Paths.get(newPath[0]);
+				
+				if (nuevaPath.toFile().exists()) {
+					this.rutaInicial = nuevaPath;
+				}else {
+					System.out.println("cd: " + nombreCarpeta + " No existe el archivo o el directorio");
+				}
 			}
-			
 		}else {
-			String nueva = rutaInicial.normalize() + "/"+ ruta;
+			String nueva = rutaInicial.normalize() + "/"+ nombreCarpeta;
 			Path nuevaPath = Paths.get(nueva);
 			
 			if (nuevaPath.toFile().exists()) {
 				this.rutaInicial = nuevaPath;
 			}else {
-				System.out.println("cd: " + ruta + " No existe el archivo o el directorio");
+				System.out.println("cd: " + nombreCarpeta + " No existe el archivo o el directorio");
 			}
 		}
 	}
+
 	
+	/**
+	 * @throws IOException
+	 * Esta funcion muestra por pantalla una breve descripcion de cada comando.
+	 */
 	public void help () throws IOException {
-		Commands comandos = new Commands ();
-		Path ruta = Paths.get("/home/alejandro/git/repository/Terminal-master/src/javanio/help");
-		Charset charset = Charset.forName("ISO-8859-1");
-		BufferedReader br = null;
-		try {
-			br = Files.newBufferedReader(ruta, charset);
-			String linea = null;
-			
-			while ((linea = br.readLine()) != null) {
-				System.out.println(linea);
-				}
-			
-			br.close();
-			} catch (IOException x) {
-				System.out.println("Escriba bien el comando.");
-				System.out.println(comandos.rutaInterfaz());
-				//System.err.format("IOException: %s%n", x);
-		}
+		System.out.println("ls: Hace un listado de los directorios y archivos de la ruta en la que se encuentra.");
+		System.out.println();
+		System.out.println("cd <nombre directorio>: Este comando permite accede a un directorio de manera que al escribir cd y el nombre de un directorio accederá a él.");
+		System.out.println();
+		System.out.println("cd : Muestra el directorio actual.");
+		System.out.println();
+		System.out.println("cd .. : Accede al directorio padre.");
+		System.out.println();
+		System.out.println("cd <nombre_directorio>: Accede a un directorio dentro del directorio actual.");
+		System.out.println();
+		System.out.println("cd <C:\\Nombre_Directorio>: Accede a una ruta absoluta del sistema.");
+		System.out.println();
+		System.out.println("dir: Lista los archivos contenidos en el directorio actual.");
+		System.out.println();
+		System.out.println("mkdir <nombre_directorio>: Crea un directorio en la ruta actual.");
+		System.out.println();
+		System.out.println("Info <nombre_archivo_o_directorio> este comando imprimirá los datos básicos de un archivo o directorio: su nombre, nombre de la carpeta padre, el tamaño del archivo o la carpeta.Acceso a datos Desarrollo web Entorno servidor Despliegue de aplicaciones web.");
+		System.out.println();
+		System.out.println("cat <nombre_fichero> : muestra el contenido de un fichero a través del terminal.");
+		System.out.println();
+		System.out.println("top <numero_lineas> <nombre_fichero>: muestras las primeras <numero_lineas> de un fichero. De manera que top 20 hola.txt muestra las primeras 20 líneas del fichero hola.txt");
+		System.out.println();
+		System.out.println("mkfile <nombre_fichero> <texto>: Crea un nuevo fichero llamado <nombre fichero> y escribe en el <texto>."); 
+		System.out.println();
+		System.out.println("write <nombre_fichero> <texto>: Añade <texto> al final de <nombre_fichero> que ya existe.");
+		System.out.println();
+		System.out.println("Delete <nombre_archivo_o_directorio>: Borra un archivo o una carpeta, en caso de ser una carpeta borra todos los archivos que la contienen.");
+		System.out.println();
+		System.out.println("dir: listas los archivos u directorios de la ruta actual.");
+		System.out.println();
+		System.out.println("dir <nombre_directorio>: listas los archivos y directorios de <nombre_directorio>-"); 
+		System.out.println();
+		System.out.println("dir <C:\\Nombre_Directorio>: listas los archivos y directorios de una ruta absoluta.");
+		System.out.println();
+		System.out.println("cp <fichero_original> <fichero_copia>: Realiza una copia del fichero denominado cp <fichero_original> al <fichero_copia>:. El <fichero_original> y/o el <fichero_copia>: se podrán especificar con el nombre de un archivo en el directorio local o una ruta absoluta al fichero. \n" + 
+				"En caso de que el nombre del <fichero_copia> ya exista se dará la opción de sobreescribirlo.");
 	}
-	
+
 	/**
 	 * @param ruta
 	 * Recibe un parámetro de tipo Path el cual lista los directorios y ficheros de la ruta en un array 
@@ -98,6 +138,7 @@ public class Commands {
 			System.out.println(listar[i]);
 		}
 	}
+	
 	public void dirArchivo (String nombreArchivo) {
 		Path rutaArchivo = Paths.get(rutaInicial + "/" + nombreArchivo);
 		String [] listar = rutaArchivo.toFile().list();
@@ -109,7 +150,6 @@ public class Commands {
 		}else {
 			System.out.println("El archivo que quiere listar no existe");
 		}
-		
 	}
 	
 	public void dirAbsolute (String rutaAbsoluta) {
@@ -125,19 +165,26 @@ public class Commands {
 		}
 	}
 	
-	
+	/**
+	 * @param nombreArchivo
+	 * Este método crea un directorio
+	 */
 	public void mkdir (String nombreArchivo) {
 		Path rutaArchivo = Paths.get(rutaInicial.toAbsolutePath() + "/"+nombreArchivo);
 
-		if (rutaArchivo.toFile().isDirectory() == true) {
-			System.out.println("mkdir: no se puede crear el directorio " + nombreArchivo +" El archivo ya existe");
-			
-		}else {
-			rutaArchivo.toFile().mkdir();
-			
+			if (rutaArchivo.toFile().isDirectory() == true) {
+				System.out.println("mkdir: no se puede crear el directorio " + nombreArchivo +" El archivo ya existe");
+			}else {
+				rutaArchivo.toFile().mkdir();
 		}
 	}
 	
+	/**
+	 * @param nombreArchivo
+	 * @throws IOException
+	 * Este comando imprimirá los datos básicos de un archivo o directorio: su nombre, nombre de la carpeta padre, el tamaño del archivo o la carpeta.
+	 * 
+	 */
 	public void info (String nombreArchivo) throws IOException {
 		Path rutaArchivo = Paths.get(rutaInicial.normalize() + "/" + nombreArchivo);
 		
@@ -154,6 +201,10 @@ public class Commands {
 		}
 	}
 	
+	/**
+	 * @param nombreFichero
+	 * Muestra el contenido de un fichero a través del terminal.
+	 */
 	public void cat (String nombreFichero) {
 		Path rutaFichero = Paths.get(rutaInicial.toAbsolutePath()+ "/" + nombreFichero);
 		Charset charset = Charset.forName("ISO-8859-1");
@@ -176,7 +227,7 @@ public class Commands {
 			System.out.println(nombreFichero + ": Es un directorio");
 		}
 	}
-
+	
 	/**
 	 * Muestras las primeras <numlineas> de un fichero.	De manera que top 20 hola.txt muestra las primeras 20 líneas del fichero hola.txt
 	 * @param numLineas
@@ -218,6 +269,11 @@ public class Commands {
 		}
 	}
 	
+	/**
+	 * @param nombreFichero
+	 * @throws IOException
+	 * Este método crea un fichero de texto
+	 */
 	public void mkfile (String nombreFichero) throws IOException {
 		Path rutaFichero = Paths.get(rutaInicial.normalize() + "/" + nombreFichero);
 		
@@ -229,6 +285,39 @@ public class Commands {
 		
 	}
 	
+	/**
+	 * @param nombreFichero
+	 * @param texto
+	 * @throws IOException
+	 * 
+	 * Este método crea un fichero de texto y imprime en él, el texto que se le pasa como argumento 
+	 */
+	public void mkfile (String nombreFichero, String [] texto) throws IOException {
+		Path rutaFichero = Paths.get(rutaInicial.normalize() + "/" + nombreFichero);
+		
+		if (rutaFichero.toFile().exists()) {
+			System.out.println("El fichero ya existe");
+		}else {
+			rutaFichero.toFile().createNewFile();
+			Charset charset = Charset.forName("ISO-8859-1");
+			try (BufferedWriter wr = Files.newBufferedWriter(rutaFichero, charset, StandardOpenOption.APPEND)) {
+				
+					for (int i = 2; i < texto.length; i++) {
+					wr.write(texto[i] + " ");
+				}
+			} catch (IOException e) {
+				System.out.println("El archivo no exite");
+				System.out.println(rutaFichero.toString());
+			}
+		}
+		
+	}
+	
+	/**
+	 * @param nombreFichero
+	 * @param texto
+	 * Añade <texto> al final de nombreFichero que ya existe.
+	 */
 	public void write (String nombreFichero, String [] texto) {
 		Path rutaFichero = Paths.get(rutaInicial.normalize() + "/" + nombreFichero);
 		Charset charset = Charset.forName("ISO-8859-1");
@@ -244,81 +333,88 @@ public class Commands {
 		}
 	}
 	
+	/**
+	 * @param nombreFichero
+	 * Este método borra el fichero o directorio con el nombre que se le pasa como argumento
+	 */
 	public void delete (String nombreFichero) {
-		Path rutaFichero = Paths.get(rutaInicial.normalize() + "/" + nombreFichero);
-		if (rutaFichero.toFile().exists()) {
-			rutaFichero.toFile().delete();
+		
+		if (nombreFichero.startsWith("/")) {
+			Path rutaFichero = Paths.get(nombreFichero);
+			if (rutaFichero.toFile().exists()) {
+				rutaFichero.toFile().delete();
+			}else {
+				System.out.println("El archivo no existe.");
+			}
 		}else {
-			System.out.println("El archivo no existe.");
-		}
-	}
-	
-	public boolean existe (String nombreArchivo) {
-		Path rutaArchivo = Paths.get(rutaInicial.normalize() + "/"+nombreArchivo);
-		return rutaArchivo.toFile().exists();
-	}
-	
-	public void cpLocal (String ficheroOriginal, String ficheroNombre) {
-		Path rutaOriginal = Paths.get(rutaInicial.normalize() + "/" + ficheroOriginal);
-		Path archivoCopiado = Paths.get(rutaInicial.normalize() + "/" + ficheroNombre);
-		
-		try {
-			Files.copy(rutaOriginal, archivoCopiado, REPLACE_EXISTING);
-		}catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
-	public void cpAbsolute (String originalAbsoluta, String copiaAbsoluta) {
-		Path rutaOriginal = Paths.get(originalAbsoluta);
-		Path rutaCopia = Paths.get(copiaAbsoluta);
-		
-		if (originalAbsoluta.startsWith("/")) {
-			try {
-				Files.copy(rutaOriginal, rutaCopia);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+			Path rutaFichero = Paths.get(rutaInicial.normalize() + "/" + nombreFichero);
+			if (rutaFichero.toFile().exists()) {
+				rutaFichero.toFile().delete();
+			}else {
+				System.out.println("El archivo no existe.");
 			}
 		}
-		try {
-			Files.copy(rutaOriginal, rutaCopia);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	}
+	
+	/**
+	 * @param nombreArchivo
+	 * @return devuelve un true si existe el archivo o fichero o un false si no existe. 
+	 */
+	public boolean existe (String nombreArchivo) {
+		if (nombreArchivo.startsWith("/")) {
+			Path rutaArchivo = Paths.get(nombreArchivo);
+			return rutaArchivo.toFile().exists();
+		}else {
+			Path rutaArchivo = Paths.get(rutaInicial.normalize() + "/"+nombreArchivo);
+			return rutaArchivo.toFile().exists();
+		}
+		
+	}
+	
+	/**
+	 * @param copiar
+	 * @param nuevaDireccion
+	 * 
+	 * Este método hace una copia a la direccion que se le pasa como segundo argumento.
+	 * Si este ya existe preguntara si quiere sobreescribirlo.
+	 */
+	public void cp(String copiar, String nuevaDireccion)  {
+		Commands comandos = new Commands ();
+		Path rutaOriginalAbsoluta = Paths.get(copiar);
+		Path rutaCopiaAbsoluta = Paths.get(nuevaDireccion);
+		
+		if (copiar.startsWith("/")) {
+			try {
+				Files.copy(rutaOriginalAbsoluta, rutaCopiaAbsoluta);
+			} catch (IOException e) {
+				e.printStackTrace();
+				comandos.rutaInterfaz();
+			}
+		}else if (nuevaDireccion.equalsIgnoreCase("/")){
+			try {
+				Files.copy(rutaOriginalAbsoluta, rutaCopiaAbsoluta);
+			} catch (IOException e) {
+				e.printStackTrace();
+				comandos.rutaInterfaz();
+			}
+		}else {
+			Path rutaOriginal = Paths.get(rutaInicial.normalize() + "/" + copiar);
+			Path archivoCopiado = Paths.get(rutaInicial.normalize() + "/" + nuevaDireccion);
+			
+			try {
+				Files.copy(rutaOriginal, archivoCopiado);
+			}catch(IOException e) {
+				e.printStackTrace();
+				comandos.rutaInterfaz();
+			}
 		}
 	}
 	
+	public Path crearPath (String nombreArchivo) {
+		Path crear = Paths.get(rutaInicial.normalize() + "/" + nombreArchivo);
+		
+		return  crear;
+	}
+	
+	
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- Terminal-master/src/javanio/mainTerminalNio.java
+++ Terminal-master/src/javanio/mainTerminalNio.java
@@ -11,62 +11,83 @@ public class mainTerminalNio {
 				
 		System.out.println(comandos.rutaInterfaz());
 		operacion = sc.nextLine();
-		
-		
+
 		do {
 			String [] separacion = operacion.split(" ");
+			int tamaño = separacion.length;
+		
 			//Comando help
 			if (separacion[0].equals("help")) {
-				try {
-					comandos.help();
-				} catch (IOException e) {
-					System.out.println("Escriba bien el comando.");
-					
+				if (tamaño > 1) {
+					System.out.println("Escriba bien el comando");
+					System.out.println(comandos.rutaInterfaz());
+				}else {
+					try {
+						comandos.help();
+						System.out.println(comandos.rutaInterfaz());
+					} catch (IOException e) {
+						System.out.println("Escriba bien el comando.");
+					}
 				}
-			}			
+			}	
+			
 			//Comando cd
 			try {
-					if (separacion[0].equals("cd")) {
+				if (separacion[0].equals("cd")) {
+					
+					if (tamaño > 1) {
 						if (separacion [1].startsWith("/")){
-							//System.out.println(comandos.cdAbsouluta(separacion [1].toString()));
 							comandos.setRutaInicial(separacion[1]);;
 							System.out.println(comandos.rutaInterfaz());
 						}else if (separacion [1].startsWith("..")) {
-							//System.out.println(comandos.cdPadre().toString());
 							comandos.setRutaInicial(separacion[1]);
 							System.out.println(comandos.rutaInterfaz());
 							
 						}else {
-							//System.out.println(comandos.cd(separacion[1].toString()));
-							comandos.setRutaInicial(separacion[1]);
-							System.out.println(comandos.rutaInterfaz());
+							if (comandos.crearPath(separacion[1]).toFile().isFile()) {
+								System.out.println("cd: " +  separacion [1] + " No es un directorio");
+								System.out.println(comandos.rutaInterfaz());
+
+							}else {
+								comandos.setRutaInicial(separacion[1]);
+								System.out.println(comandos.rutaInterfaz());
+							}
+							
 						}
-				}	
-			}catch (ArrayIndexOutOfBoundsException e) {
-				comandos.setRutaInicial(separacion[1]);
-				System.out.println(comandos.rutaInterfaz());
-				
-			}
+					}else {
+						System.out.println(comandos.rutaInterfaz());
+					}
+					
+			}	
+		}catch (ArrayIndexOutOfBoundsException e) {
+			comandos.setRutaInicial(separacion[1]);
+			System.out.println(comandos.rutaInterfaz());
 			
-			
+		}
+
 			//Comando dir
 			if (separacion[0].equals("dir")) {
-				int tamaño = separacion.length;
 				
-				if (tamaño > 1) {
-					if (separacion [1].startsWith("/")) {
-						comandos.dirAbsolute(separacion [1]);
-						System.out.println(comandos.rutaInterfaz());
+				if (tamaño < 3) {
+					if (tamaño > 1) {
+						if (separacion [1].startsWith("/")) {
+							comandos.dirAbsolute(separacion [1]);
+							System.out.println(comandos.rutaInterfaz());
+						}else {
+							comandos.dirArchivo(separacion [1]);
+							System.out.println(comandos.rutaInterfaz());
+						}
 					}else {
-						comandos.dirArchivo(separacion [1]);
+						comandos.dir();
 						System.out.println(comandos.rutaInterfaz());
 					}
 				}else {
-					comandos.dir();
+					System.out.println("El comando ha sido mal escrito.");
 					System.out.println(comandos.rutaInterfaz());
 				}
+
 			}
-			 
+			
 			//Comando mkdir
 			if (separacion [0].equals("mkdir")) {
 				comandos.mkdir(separacion[1].toString());
@@ -86,19 +107,33 @@ public class mainTerminalNio {
 			}
 			
 			//Comando top
-			if (separacion [0].equals("top")) {
-				comandos.top(separacion [1], separacion [2]);
-				System.out.println(comandos.rutaInterfaz());
+			if (separacion [0].equalsIgnoreCase("top")) {
+				if (tamaño < 3) {
+					System.out.println("El comando top debe tener 2 argumentos. Por ejemplo: top 5 hola. Siendo hola el nombre del fichero.");
+					System.out.println(comandos.rutaInterfaz());
+				}else {
+					comandos.top(separacion [1], separacion [2]);
+					System.out.println(comandos.rutaInterfaz());
+					
+				}
 			}
+			
 			
 			//Comando mkfile
 			if (separacion [0].equals("mkfile")) {
-				try {
-					comandos.mkfile(separacion[1]);
+				
+				if (tamaño > 2) {
+					comandos.mkfile(separacion[1], separacion);
 					System.out.println(comandos.rutaInterfaz());
-				} catch (IOException e) {
-					e.printStackTrace();
+				}else {
+					try {
+						comandos.mkfile(separacion[1]);
+						System.out.println(comandos.rutaInterfaz());
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
 				}
+				
 			}
 			
 			
@@ -117,31 +152,53 @@ public class mainTerminalNio {
 			
 			//Comando cp
 			if (separacion [0].equals("cp")) {
-				int tam = separacion.length;
-				if (tam > 2) {
-					if (comandos.existe(separacion [2])) {
-						System.out.println("El archivo o fichero que quiere copiar ya existe, ¿Desea sobreescribirlo?");
-						System.out.println("Introduzca Si o No");
-						String confirmacion = sc.nextLine();
+				
+				if (comandos.existe(separacion[1]) == false) {
+					System.out.println("El archivo que quiere copiar no existe");
+					System.out.println(comandos.rutaInterfaz());
+				}else if (comandos.existe(separacion [2])) {
+						System.out.println("El archivo que quiere copiar ya existe, desea sobreescribirlo? \n"
+								+ "Escriba Si o no.");
+						operacion = sc.nextLine();
 						
-						while (confirmacion.equalsIgnoreCase("si") || confirmacion.equalsIgnoreCase("no")) {
-							if (confirmacion.equalsIgnoreCase("si")) {
-								comandos.cpLocal(separacion [1], separacion [2]);
-							}else {
-								System.out.println("Operacion Cancelada");
-							}
+						while (operacion.equalsIgnoreCase("si") == false && operacion.equalsIgnoreCase("no") == false)  {
+							System.out.println("Escriba Si o No.");
+							operacion = sc.nextLine();
 						}
-					}else {
-						System.out.println("El fichero no existe");
-						System.out.println(comandos.rutaInterfaz());
-					}
+							
+						if (operacion.equalsIgnoreCase("si")) {
+							comandos.delete(separacion [2]);
+							comandos.cp(separacion [1], separacion [2]);
+							System.out.println(comandos.rutaInterfaz());
+						}else {
+							System.out.println("Operacion Cancelada.");
+						}
+				}else {
+					comandos.cp(separacion [1], separacion [2]);
+					System.out.println(comandos.rutaInterfaz());
 				}
-			}else {
-				System.out.println("Comando no encontrado.");
-				System.out.println(comandos.rutaInterfaz());
+			}
+			
+			if (separacion [0].equals("help") == false || separacion [0].equals("cd") == false || separacion [0].equals("dir") == false || 
+				separacion [0].equals("mkdir") == false || separacion [0].equals("info") == false || separacion [0].equals("cat") == false ||
+				separacion [0].equals("cat") == false || separacion [0].equals("top") == false || separacion [0].equals("mkfile") == false ||
+				separacion [0].equals("write") == false || separacion [0].equals("delete") == false || separacion [0].equals("cp") == false){
+				
+				if (separacion [0].equals("help") || separacion [0].equals("cd") || separacion [0].equals("dir") || 
+					separacion [0].equals("mkdir") || separacion [0].equals("info") || separacion [0].equals("cat") ||
+					separacion [0].equals("cat") || separacion [0].equals("top") || separacion [0].equals("mkfile") ||
+					separacion [0].equals("write") || separacion [0].equals("delete") || separacion [0].equals("cp")) {
+					
+				}else {
+					System.out.println("Comando no encontrado.");
+					System.out.println(comandos.rutaInterfaz());
+				}
+				
+				
 			}
 			operacion = sc.nextLine();
 		}while (operacion.equalsIgnoreCase("exit") == false);
+		
 		
 		sc.close();
 	}
